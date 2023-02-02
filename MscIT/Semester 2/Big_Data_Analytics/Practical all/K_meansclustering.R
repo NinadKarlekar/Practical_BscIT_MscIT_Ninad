5f2e2b5ca68e9ab7d69df6c205ea2f0ddd0f0794
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2023-02-03 00:03:24 +0530

    Sem 2 BDA updated

---
+++ MscIT/Semester 2/Big_Data_Analytics/Practical all/K_meansclustering.R
@@ -0,0 +1,40 @@
+# install required packages
+install.packages("plyr")
+install.packages("ggplot2")
+install.packages("cluster")
+install.packages("lattice")
+install.packages("grid")
+install.packages("gridExtra")
+# Load the package
+library(plyr)
+library(ggplot2)
+library(cluster)
+library(lattice)
+library(grid)
+library(gridExtra)
+# A data frame is a two-dimensional array-like structure in which each column contains values of one variable and each row contains one set of values from each column.
+grade_input=as.data.frame(read.csv("F:/GitHub/Practical_BscIT_MscIT_Ninad/MscIT/Semester 2/Big_Data_Analytics/Dataset/grades_km_input.csv"))
+kmdata_orig=as.matrix(grade_input[, c ("Student","English","Math","Science")])
+kmdata=kmdata_orig[,2:4]
+kmdata[1:10,]
+# the k-means algorithm is used to identify clusters for k = 1, 2, .. . , 15. For each value of k, the WSS is calculated.
+wss=numeric(15)
+# the option n start=25 specifies that the k-means algorithm will be repeated 25 times, each starting with k random initial centroids
+for(k in 1:15)wss[k]=sum(kmeans(kmdata,centers=k,nstart=25)$withinss)
+plot(1:15,wss,type="b",xlab="Number of Clusters",ylab="Within sum of square")
+#As can be seen, the WSS is greatly reduced when k increases from one to two. Another substantial reduction in WSS occurs at k = 3. However, the improvement in WSS is fairly linear fork > 3.
+km = kmeans(kmdata,3,nstart=25)
+km
+c( wss[3] , sum(km$withinss))
+df=as.data.frame(kmdata_orig[,2:4])
+df$cluster=factor(km$cluster)
+centers=as.data.frame(km$centers)
+
+g1=ggplot(data=df, aes(x=English, y=Math, color=cluster )) + geom_point() + theme(legend.position="right") + geom_point(data=centers,aes(x=English,y=Math, color=as.factor(c(1,2,3))),size=10, alpha=.3, show.legend =FALSE)
+
+g2=ggplot(data=df, aes(x=English, y=Science, color=cluster )) + geom_point () +geom_point(data=centers,aes(x=English,y=Science, color=as.factor(c(1,2,3))),size=10, alpha=.3, show.legend=FALSE)
+
+g3 = ggplot(data=df, aes(x=Math, y=Science, color=cluster )) + geom_point () + geom_point(data=centers,aes(x=Math,y=Science, color=as.factor(c(1,2,3))),size=10, alpha=.3, show.legend=FALSE)
+tmp=ggplot_gtable(ggplot_build(g1))
+
+grid.arrange(arrangeGrob(g1 + theme(legend.position="none"),g2 + theme(legend.position="none"),g3 + theme(legend.position="none"),top ="High School Student Cluster Analysis" ,ncol=1))
