ea8ee1bc1cb765cb15c88b92adfb2348b7d7d637
GrimReaperkn <ninadkarlekar0001@gmail.com>
2023-03-23 01:24:52 +0530

    j

---
+++ MscIT/Semester 2/MicroservicesArchitecture/Practical 2/NameRollNoMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NameRollNoMVC.Models;
+namespace NameRollNoMVC.Controllers
+{
+    public class HomeController: Controller
+    {
+        public async Task < IActionResult > Index()
+        {
+            var model = new StockQuote
+            {
+                Name = "Ninad", RollNo = "22306A1012"
+            };
+            return View(model);
+        }
+    }
+}
