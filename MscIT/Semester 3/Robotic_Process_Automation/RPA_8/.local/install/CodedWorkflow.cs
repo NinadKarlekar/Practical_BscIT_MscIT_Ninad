881b1e89610195ec46807d45d2c9665f06cfca81
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2023-12-22 02:57:24 +0530

    RPA and ML Done All

--- MscIT/Semester 3/Robotic_Process_Automation/RPA_8/.local/install/CodedWorkflow.cs
+++ MscIT/Semester 3/Robotic_Process_Automation/RPA_8/.local/install/CodedWorkflow.cs
@@ -19,7 +19,7 @@ namespace RPA_8
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
