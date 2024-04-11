5863addb3a23467e508b768069b7293f3d02ecfe
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2024-04-12 00:03:11 +0530

    Create BC_1ab.py

---
+++ MscIT/Semester 4/Blockchain/Practical01/BC_1ab.py
@@ -0,0 +1,79 @@
+#pip install cryptodome  OR
+#pip install pycryptodome
+
+import Crypto
+import binascii
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5
+
+class Client:
+    def __init__(self):
+        # Creating random number for key
+        random = Crypto.Random.new().read
+        # Creating new public key and private key
+        self._private_key = RSA.generate(1024, random)
+        self._public_key = self._private_key.publickey()
+        self._signer = PKCS1_v1_5.new(self._private_key)
+        
+    @property
+    def identity(self):
+        return binascii.hexlify(self._public_key.exportKey(format='DER')).decode('ascii')
+        
+Demo = Client()
+print(Demo.identity)
+
+
+
+
+###########################
+
+
+import datetime
+import collections
+import binascii
+
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+
+class Transaction:
+    def __init__(self, sender, receiver, value):
+        self.sender = sender
+        self.receiver = receiver
+        self.value = value
+        self.time = datetime.datetime.now()
+
+    def to_dict(self):
+        if self.sender == "Genesis":
+            identity = "Genesis"
+        else:
+            identity = self.sender.identity
+
+        return collections.OrderedDict({
+            'sender': identity,
+            'receiver': self.receiver,
+            'value': self.value,
+            'time': self.time
+        })
+
+    def sign_transaction(self):
+        private_key = self.sender._private_key
+        signer = PKCS1_v1_5.new(private_key)
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
+
+Ninad = Client()
+print("Ninad Key")
+print(Ninad.identity)
+
+KS = Client()
+print("KS Key")
+print(KS.identity)
+
+t = Transaction(Ninad, KS.identity, 10.0)
+print("Transaction Sign")
+signature = t.sign_transaction()
+print(signature)
