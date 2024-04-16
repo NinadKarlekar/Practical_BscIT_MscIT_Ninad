ed88d439bdb73d37522fc094717ef16afe8ff8ad
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2024-04-16 22:41:12 +0530

    Update DL_2.py

--- MscIT/Semester 4/Deep_Learning/Practical02/DL_2.py
+++ MscIT/Semester 4/Deep_Learning/Practical02/DL_2.py
@@ -1,15 +1,22 @@
 import numpy as np
-from keras.layers import Dense
 from keras.models import Sequential
+from keras.layers import Dense
 
 model = Sequential()
 model.add(Dense(units=2, activation="relu", input_dim=2))
 model.add(Dense(units=1, activation="sigmoid"))
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-print(model.summary())
-print(model.get_weights())
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics="accuracy")
+
 X = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
-Y = np.array([0.0, 1.0, 1.0, 0.0])
-model.fit(X, Y, epochs=1000, batch_size=4)
-print(model.get_weights())
-print(model.predict(X, batch_size=4))
+print("Input data:")
+print(X)
+
+y = np.array([0.0, 1.0, 1.0, 0.0])
+print("\nTarget labels:")
+print(y)
+
+model.get_weights()
+model.fit(X, y, epochs=500)
+predictions = model.predict(X)
+print("\nPredictions after training:")
+print(predictions)
