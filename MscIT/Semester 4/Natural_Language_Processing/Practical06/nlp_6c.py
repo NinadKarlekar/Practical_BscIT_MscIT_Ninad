bb073b6078389e6f9de1f7ed3a6a8427ca7a5b3b
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2024-06-16 13:48:46 +0530

    Create nlp_6c.py

---
+++ MscIT/Semester 4/Natural_Language_Processing/Practical06/nlp_6c.py
@@ -0,0 +1,11 @@
+# 6C: Named Entity recognition with diagram using NLTK corpus - treebank
+
+import nltk
+nltk.download('treebank')
+from nltk.corpus import treebank_chunk
+treebank_chunk.tagged_sents()[0]
+treebank_chunk.chunked_sents()[0]
+treebank_chunk.chunked_sents()[0].draw()
+
+
+# Note: It runs on Python IDLE, VScode#
