e319e0f630b87e84ba0068f9b9df1248b7824ecd
Ninad Karlekar <ninad.karlekar@vsit.edu.in>
2024-06-16 13:16:09 +0530

    Create nlp_2g_1.py

---
+++ MscIT/Semester 4/Natural_Language_Processing/Practical02/nlp_2g_1.py
@@ -0,0 +1,15 @@
+import nltk
+from nltk.tag import DefaultTagger
+from nltk.corpus import treebank
+
+# Create a default tagger that tags everything as 'NN'
+exptagger = DefaultTagger('NN')
+
+# Get test sentences from the treebank corpus
+testsentences = treebank.tagged_sents()[1000:]
+
+# Evaluate the tagger on the test sentences
+print(exptagger.evaluate(testsentences))
+
+## Tagging a list of sentences
+print(exptagger.tag_sents([['Hi', ','], ['How', 'are', 'you', '?']]))
